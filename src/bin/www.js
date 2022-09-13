86688a0d642125d7a698691d74b37ccb5ff7ae09
= <=>
2022-09-13 02:15:40 +0100

    initial commit

---
+++ src/bin/www.js
@@ -0,0 +1,10 @@
+import http from "http";
+import app from "../app";
+
+const PORT = process.env.PORT || 3001;
+
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+  console.log("sever is listening on port " + PORT);
+});
